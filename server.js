868f8f17fbdf5879ea0d95cb07aeabf10bb4fc78
Faroque-Hasnat <42097019+Faroque-Hasnat@users.noreply.github.com>
2019-09-04 21:30:16 +0600

    "app.use" error solve

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/users', userRouter)
 app.use('/api/transactions', transactionRouter)
 
 if(process.env.NODE_ENV === 'production') {
-   app.user(express.static('client/build'))
+   app.use(express.static('client/build'))
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
    })
