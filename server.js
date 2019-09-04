d24c1e542d1af13c24478938745f26b11b99168c
Faroque-Hasnat <42097019+Faroque-Hasnat@users.noreply.github.com>
2019-09-04 21:55:18 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/transactions', transactionRouter)
 if(process.env.NODE_ENV === 'production') {
    app.use(express.static('client/build'))
    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', index.html))
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
    })
 }
 
